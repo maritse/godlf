5d6bf5b90a02bf17f0bbacb5c08adeb3c94fe486
maritse <martinmatyja1724@gmail.com>
2024-03-19 22:48:49 +0000

    update activ func; still draft

--- neural/activation_func.go
+++ neural/activation_func.go
@@ -12,3 +12,25 @@ func sigmoidDerivative(value float64) float64 {
 	// calculation -> https://math.stackexchange.com/questions/78575/derivative-of-sigmoid-function-sigma-x-frac11e-x
 	return sigmoid(value) * (1.0 - sigmoid(value))
 }
+
+func ReLU(value float64) float64 {
+	if value > 0 {
+		return value
+	}
+	return 0
+}
+
+func ReLUDerivative(value float64) float64 {
+	if value < 0 {
+		return value
+	}
+	return 0
+}
+
+func linear(value float64) float64 {
+	return value
+}
+
+func linearDerivative(value float64) float64 {
+	return 1
+}
